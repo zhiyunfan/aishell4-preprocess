9f374565e4786e192a0a8508e6b19b91f4c56dc4
fanzhiyun <fanzhiyun2017@ia.ac.cn>
2022-04-07 21:12:50 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,6 +6,12 @@ if [ ! -d $outputdir ];then
   mkdir -p $outputdir
 fi
 
+## overlap_spk_tolerance: the max number of speakers speaking at the same time
+## overlap_tolerance: the max number of speaker overlapped frames in one generated sentences
+## overrate_tolerance: max duration ratio of overlapped speech in any interval
+## adjacent_segments: the number of intervals used to form one sentence
+## sil_tolerance: the max duration of silence
+
 python -u grep_aishell4_multi_spk.py  --output_dir $outputdir \
          --aishell4_wav_list $aishell4wav --textgrid_list $aishell4text  \
          --overlap_spk_tolerance  2  --overlap_tolerance 10 --overrate_tolerance 0.1 \
